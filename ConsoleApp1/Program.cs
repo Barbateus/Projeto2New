78e4690380e6357b57c8a1501cd0a781d621b6d5
Matteus Barbosa <matteus.barbosa@yahoo.com.br>
2023-07-21 01:11:06 -0300

    Gerador de senha com while

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,15 +7,22 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Informe a quantidade de digitos da senha (entre 4 e 10, pares): ");
-            var quantidadeDigitos = Convert.ToInt32(Console.ReadLine());
+            //var quantidadeDigitos = Convert.ToInt32(Console.ReadLine());
+
+            var quantidadeDigitos = 0;
+
+            while (quantidadeDigitos == 0)
+            {
+                quantidadeDigitos = ObterQuantidadeDigitos();
+            }
 
             //if (quantidadeDigitos < 4 || quantidadeDigitos > 10 || quantidadeDigitos %2 !=0) 
-            if (quantidadeDigitos is < 4 or > 10 || quantidadeDigitos % 2 != 0)
-            {
-                Console.WriteLine($"O valor {quantidadeDigitos} é inválido de acordo com as regras.");
-                Console.ReadKey();
-                return;
-            }
+            //if (quantidadeDigitos is < 4 or > 10 || quantidadeDigitos % 2 != 0)
+            //{
+            //    Console.WriteLine($"O valor {quantidadeDigitos} é inválido de acordo com as regras.");
+            //    Console.ReadKey();
+            //    return;
+            //}
 
             var senha = "";
             var randomico = new Random();
@@ -29,6 +36,19 @@ namespace ConsoleApp1
             }
 
             Console.WriteLine($"Senha gerada: {senha}");
+        }
+
+        private static int ObterQuantidadeDigitos()
+        {
+            int.TryParse(Console.ReadLine(), out int quantidadeDigitos);
+
+            if (quantidadeDigitos is < 4 or > 10 || quantidadeDigitos % 2 != 0)
+            {
+                Console.WriteLine($"O valor {quantidadeDigitos} é inválido de acordo com as regras.");
+                quantidadeDigitos = 0;
+            }
+
+            return quantidadeDigitos;
         }
     }
 }
