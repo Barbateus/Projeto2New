5a92661a5248db228cdbde53f08bd7469e092aaa
Matteus Barbosa <matteus.barbosa@yahoo.com.br>
2023-07-20 21:48:54 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a quantidade de digitos da senha (entre 4 e 10, pares): ");
+            var quantidadeDigitos = Convert.ToInt32(Console.ReadLine());
+
+            //if (quantidadeDigitos < 4 || quantidadeDigitos > 10 || quantidadeDigitos %2 !=0) 
+            if (quantidadeDigitos is < 4 or > 10 || quantidadeDigitos % 2 != 0)
+            {
+                Console.WriteLine($"O valor {quantidadeDigitos} é inválido de acordo com as regras.");
+                Console.ReadKey();
+                return;
+            }
+
+            var senha = "";
+            var randomico = new Random();
+
+            for (int i = 0; i < quantidadeDigitos; i++)
+            {
+                //var algarismo = randomico.Next(1, 61);
+                var algarismo = randomico.Next(10);
+
+                senha += algarismo;
+            }
+
+            Console.WriteLine($"Senha gerada: {senha}");
+        }
+    }
+}
