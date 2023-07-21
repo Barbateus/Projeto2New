d5fb65d5c4e40dbe48c78904186f5b1874f3d779
Matteus Barbosa <matteus.barbosa@yahoo.com.br>
2023-07-21 01:32:27 -0300

    Gerador de senha com do/while

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,23 +6,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Informe a quantidade de digitos da senha (entre 4 e 10, pares): ");
-            //var quantidadeDigitos = Convert.ToInt32(Console.ReadLine());
+            int quantidadeDigitos;
 
-            var quantidadeDigitos = 0;
-
-            while (quantidadeDigitos == 0)
+            do
             {
+                Console.WriteLine("Informe a quantidade de digitos da senha (entre 4 e 10, pares): ");
                 quantidadeDigitos = ObterQuantidadeDigitos();
-            }
-
-            //if (quantidadeDigitos < 4 || quantidadeDigitos > 10 || quantidadeDigitos %2 !=0) 
-            //if (quantidadeDigitos is < 4 or > 10 || quantidadeDigitos % 2 != 0)
-            //{
-            //    Console.WriteLine($"O valor {quantidadeDigitos} é inválido de acordo com as regras.");
-            //    Console.ReadKey();
-            //    return;
-            //}
+            } while (quantidadeDigitos == 0);
 
             var senha = "";
             var randomico = new Random();
@@ -42,6 +32,7 @@ namespace ConsoleApp1
         {
             int.TryParse(Console.ReadLine(), out int quantidadeDigitos);
 
+            //if (quantidadeDigitos < 4 || quantidadeDigitos > 10 || quantidadeDigitos %2 !=0)
             if (quantidadeDigitos is < 4 or > 10 || quantidadeDigitos % 2 != 0)
             {
                 Console.WriteLine($"O valor {quantidadeDigitos} é inválido de acordo com as regras.");
